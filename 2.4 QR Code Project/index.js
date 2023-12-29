34daa32e57ac67cbda787062a69e45be6032f25c
Quyen Ly <quyen.ly2@dxc.com>
2023-12-29 22:06:18 +0700

    clean code

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -2,15 +2,15 @@ import inquerer from "inquirer";
 import qr_image from "qr-image";
 import fs from "fs";
 
-inquerer
-    .prompt([{
-        message: "Input something ...",
-        name: "messsage"
-    }])
-    .then((ans) => {
-        const qr = qr_image.image(ans.messsage);
-        qr.pipe(fs.createWriteStream("a.png"));
-    }).catch(err => {
-        console.log("🚀 ~ file: index.js:19 ~ .then ~ catch:", err);
-    });
+const ans = await inquerer.prompt([{ name: "message", message: "input something" }]);
 
+// to qr
+const qr = qr_image.image(`${ans.message}`);
+qr.pipe(fs.createWriteStream(`${ans.message}.png`));
+
+//to file
+fs.writeFile(`${ans.message}.txt`, `${ans.message}`, error => {
+    if (error) {
+        console.log("🚀 ~ file: index.js:14 ~ error:", error);
+    }
+});
